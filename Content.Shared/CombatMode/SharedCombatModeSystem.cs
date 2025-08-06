1b825806e31eba9908b2c3b3921fe7701be46dbb
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-08-06 16:05:23 +0300

    [Port] Combat Indicator (#30)

--- Content.Shared/CombatMode/SharedCombatModeSystem.cs
+++ Content.Shared/CombatMode/SharedCombatModeSystem.cs
@@ -1,5 +1,8 @@
 using Content.Shared.Actions;
+using Content.Shared.Bed.Sleep;
 using Content.Shared.Mind;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Systems;
 using Content.Shared.MouseRotator;
 using Content.Shared.Mech.Components;
 using Content.Shared.Movement.Components;
@@ -15,6 +18,7 @@ public abstract partial class SharedCombatModeSystem : EntitySystem
     [Dependency] private   SharedActionsSystem _actionsSystem = default!;
     [Dependency] private   SharedPopupSystem _popup = default!;
     [Dependency] private   SharedMindSystem  _mind = default!;
+    [Dependency] private   MobStateSystem _mobState = default!; // Arcane
 
     public override void Initialize()
     {
@@ -46,8 +50,10 @@ public abstract partial class SharedCombatModeSystem : EntitySystem
         args.Handled = true;
         SetInCombatMode(uid, !component.IsInCombatMode, component);
 
+        /* Arcane-Edit-Start
         var msg = component.IsInCombatMode ? "action-popup-combat-enabled" : "action-popup-combat-disabled";
         _popup.PopupClient(Loc.GetString(msg), args.Performer, args.Performer);
+        */ // Arcane-Edit-End
     }
 
     public void SetCanDisarm(EntityUid entity, bool canDisarm, CombatModeComponent? component = null)
@@ -70,6 +76,14 @@ public abstract partial class SharedCombatModeSystem : EntitySystem
 
         if (component.IsInCombatMode == value)
             return;
+
+        // Arcane-Start | Dont let entity gone postal when unconscious
+        if (_mobState.IsDead(entity) || _mobState.IsCritical(entity) || HasComp<SleepingComponent>(entity))
+        {
+            if (value)
+                return;
+        }
+        // Arcane-End
 
         component.IsInCombatMode = value;
         Dirty(entity, component);
