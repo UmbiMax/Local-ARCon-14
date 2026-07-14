k0tys/Orion-Station-14

[Tweak] D.O.O.R (#427)
* Update ``DoorComponent`` to use TimeSpans and fix comments (#40420)

Cleanup

* Fix emag sparking animation on doors (#40350)

* Fix

* Update

* Comment

* fix: allow opening doors with open wire panels (#38733)

* fix: allow opening doors with open wire panels

* fix: set wire panel sprite state on airlock close

* fix: make airlock wire panels animate better

* removed unused dependency

---------

Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

* Port door animation fix from ephemeral space (#43613)

* port ES animation fix

* close sound too :)

* pump up the jam

* worlds most minor sloptimization

* silly

---------

Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

* time animations

* sounds

* another time for WINDOORs from TG13

* 1.1

* 1.2 BUGFIX fix door desynchronization

* Minor door animation tweak (#43708)

is this bad

* Cherry-pick additions

* 2 NEW sound for blastdoor

* 3 Best firelock upgrade

* 3.1

* 3.2

* 4. Sound for FORCE OPEN

* 4,1 fix

* 4,2 fix

* 5. Sound for FIRELOCKs

* 5.1

* BUNCH OF FIXES: START

* FIX

* Update SharedDoorSystem.cs

* FIX

* FIX DOUBLE DOORS

* Fix

* DOOR FIX

* Final sprite UPGRADE

* Final firelock sound UPGRADE

* FIX JITTERING. Removed small part of wizden code for optimization

* AA

* Revert "AA"

This reverts commit 25976e100e8b35393a0552e9eee37b679f3c54f8.

* Reapply "AA"

This reverts commit 7c24bfbddbeeba3052064eafd9191ebadd8b6c83.

---------

Co-authored-by: Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
Co-authored-by: Perry Fraser <perryprog@users.noreply.github.com>
Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
Co-authored-by: Jessica M <jessica@jessicamaybe.com>

Content.Shared/Access/Systems/AccessReaderSystem.cs
@@ -189,7 +189,7 @@ public sealed class AccessReaderSystem : EntitySystem
         args.Handled = true;
         accessReader.Value.Comp.AccessLists.Clear();
         accessReader.Value.Comp.AccessLog.Clear();
-        Dirty(uid, reader);
+        Dirty(accessReader.Value); // Orion-Edit: fix door desynchronization
     }
 
     /// <summary>

UmbiMax/Local-ARCon-14

Content.Shared/Access/Systems/AccessReaderSystem.cs
@@ -163,7 +163,7 @@ public sealed partial class AccessReaderSystem : EntitySystem
         args.Handled = true;
         accessReader.Value.Comp.AccessLists.Clear();
         accessReader.Value.Comp.AccessLog.Clear();
-        Dirty(uid, reader);
+        Dirty(accessReader.Value); // Arcane-Edit: fix door desynchronization
     }
 
     private void OnConfigurationAttempt(Entity<AccessReaderComponent> ent, ref AccessReaderConfigurationAttemptEvent args)