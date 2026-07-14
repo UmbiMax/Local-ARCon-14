FluxS-Dev/Amour-reborn-goob

[Port] Combat Mode Sound (#154)
Co-authored-by: nukkuminen <90336027+oldschoolotaku@users.noreply.github.com>

Content.Client/Options/UI/Tabs/AudioTab.xaml.cs
@@ -121,6 +121,7 @@ public sealed partial class AudioTab : Control
             _cfg.GetCVar(CCVars.MinMaxAmbientSourcesConfigured),
             _cfg.GetCVar(CCVars.MaxMaxAmbientSourcesConfigured));
 
+        Control.AddOptionCheckBox(CCVars.CombatModeSoundEnabled, CombatModeSoundCheckBox); // Orion
         Control.AddOptionCheckBox(CCVars.LobbyMusicEnabled, LobbyMusicCheckBox);
         Control.AddOptionCheckBox(CCVars.RestartSoundsEnabled, RestartSoundsCheckBox);
         Control.AddOptionCheckBox(CCVars.EventMusicEnabled, EventMusicCheckBox);

UmbiMax/Local-ARCon-14

Content.Client/Options/UI/Tabs/AudioTab.xaml.cs
@@ -61,6 +61,8 @@ public sealed partial class AudioTab : Control
             _cfg.GetCVar(CCVars.MinMaxAmbientSourcesConfigured),
             _cfg.GetCVar(CCVars.MaxMaxAmbientSourcesConfigured));
 
+        Control.AddOptionCheckBox(CCVars.CombatModeSoundEnabled, CombatModeSoundCheckBox); // Arcane
+
         // Starlight start
 
         Control.AddOptionPercentSlider(