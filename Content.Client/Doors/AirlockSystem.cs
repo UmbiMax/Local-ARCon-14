7c6ae341971af5dfc68732760a30429bfd54d4af
UmbiMax <maxim1213141@gmail.com>
2026-07-18 23:06:31 +0500

    Revert "replace"
    This reverts commit 672edef9804206edb310c47fbe25539397188664.

--- Content.Client/Doors/AirlockSystem.cs
+++ Content.Client/Doors/AirlockSystem.cs
@@ -117,7 +117,7 @@ public sealed partial class AirlockSystem : SharedAirlockSystem
                 ||  state == DoorState.Opening
                 ||  state == DoorState.Denying
                 || (state == DoorState.Open && comp.OpenUnlitVisible)
-                || (state == DoorState.Closed && comp.OpenUnlitVisible)) // Arcane-Edit
+                || (_appearanceSystem.TryGetData<bool>(uid, DoorVisuals.ClosedLights, out var closedLights, args.Component) && closedLights))
                     && !boltedVisible && !emergencyLightsVisible;
         }
 
