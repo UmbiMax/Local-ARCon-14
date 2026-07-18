d8e75efbc745554bb9f6969149b25d0212ae83d2
UmbiMax <maxim1213141@gmail.com>
2026-07-18 19:17:01 +0500

    Cleanup

--- Content.Shared/_Arcane/CombatMode/CombatModeEvents.cs
+++
@@ -1,8 +0,0 @@
-/*// SPDX-FileCopyrightText: 2026 PuroSlavKing <puroslavking@yahoo.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-// ReSharper disable once CheckNamespace
-namespace Content.Shared.CombatMode;
-
-public readonly record struct CombatModeChangedEvent(bool InCombatMode); */
